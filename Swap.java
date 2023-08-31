a9311a60003d5c4511ade967f5faa14a81c7f410
jrathore07 <143330597+jrathore07@users.noreply.github.com>
2023-08-31 19:27:17 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,21 @@
+//jaydeep rathore
+//0827CI221071
+import java.util.Scanner;
+public class Swap
+{
+    
+	public static void main(String[] args) {
+	    
+	    Scanner r = new Scanner(System.in);
+	    	System.out.println( "first no. is");
+	    
+	    int a=r.nextInt();
+	    	System.out.println("second no. is");
+	    int b=r.nextInt();
+	    int x;
+	    x=b;
+	    
+	    	System.out.println("After swapping \n first no. is " + x);
+	    	System.out.println("\n second no.is " +a);
+	    	    	}
+}
