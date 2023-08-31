befa0e5a8daf3111d397d9ec019b9ee05f4c1d3d
jrathore07 <143330597+jrathore07@users.noreply.github.com>
2023-08-31 19:36:24 +0530

    Add files via upload

---
+++ Check.java
@@ -0,0 +1,20 @@
+//jaydeep rathore
+//0827CI221071
+import java.util.Scanner;
+public class Check{
+    	public static void main(String[] args) {
+	    
+	    Scanner r = new Scanner(System.in);
+	    	System.out.println( "enter the value of a is:- ");
+	         int a=r.nextInt();
+	         if(a%2==0)
+	         {
+	             System.out.println("number is even ");
+	             
+	         }
+	         else{
+	             System.out.println("number is odd");
+	             
+	         }
+}
+}
