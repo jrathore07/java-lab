b5a9bae10267146df98dfe1ff0a181a322bcb0b0
jrathore07 <143330597+jrathore07@users.noreply.github.com>
2023-08-31 19:55:57 +0530

    Add files via upload

---
+++ Check2.java
@@ -0,0 +1,19 @@
+//jaydeep rathore
+//0827CI221071
+import java.util.Scanner;
+public class Check2{
+     public static void main(String[] args) {
+	    
+	    Scanner r = new Scanner(System.in);
+	    System.out.println( "enter the number :- ");
+	    int n=r.nextInt();
+	    if(n>0)
+	    {
+	        System.out.println("number is positive");
+	    }
+	    else if(n<0)
+	    {
+	        System.out.println("number is negative");
+	    }
+}
+}
