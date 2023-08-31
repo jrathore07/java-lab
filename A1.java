a816b049d1de0a39688bdbb0c5145c9ac15c284c
jrathore07 <143330597+jrathore07@users.noreply.github.com>
2023-08-31 12:27:28 +0530

    Add files via upload

---
+++ A1.java
@@ -0,0 +1,16 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+    import java.util.Scanner;
+public class A1{
+    public static void main(String[]args){
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        System.out.println("the number is "+n);
+    }
+
+}
