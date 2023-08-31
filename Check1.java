e125b52493eb63ff2020b819a70c956accf8f4c7
jrathore07 <143330597+jrathore07@users.noreply.github.com>
2023-08-31 19:46:00 +0530

    Add files via upload

---
+++ Check1.java
@@ -0,0 +1,19 @@
+//jaydeep rathore
+//0827CI221071
+import java.util.Scanner;
+public class Check1{
+    public static void main(String[] args) {
+	    
+	    Scanner r = new Scanner(System.in);
+	    System.out.println( "enter the alphabet :- ");
+	    char ch=r.nextChar();
+	    if(ch==a||ch==e||ch==i||ch==o||ch==u)
+	    {
+	         System.out.println("alphabet is vowel");
+	        
+	    }
+	    else{
+	         System.out.println("alphabet is consonent");
+	        
+	    }
+}
