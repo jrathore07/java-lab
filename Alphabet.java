397765169188ca6ee8f95e70fddc182b903d0c5e
jrathore07 <143330597+jrathore07@users.noreply.github.com>
2023-09-01 11:04:54 +0530

    Add files via upload

---
+++ Alphabet.java
@@ -0,0 +1,12 @@
+//jaydeep rathore
+//0827CI221071
+import java.util.Scanner;
+public class Alphabet{
+    public static void main(String[]args)
+    {
+        for(char c='A';c<='Z';c++)
+        {
+            System.out.print(c+ " ");
+        }
+    }
+}
