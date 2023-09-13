ead01853e120ca27512aeaae70a17ffae3175228
jrathore07 <143330597+jrathore07@users.noreply.github.com>
2023-09-13 12:50:38 +0530

    Add files via upload

---
+++ Findstring.java
@@ -0,0 +1,22 @@
+//compare two string
+import java.util.Scanner;
+public class Findstring
+{
+	public static void main(String[] args) {
+	    String a="umbrella";
+	    boolean per=false;
+	    for(int i=0;i<a.length();i++)
+	    {
+	        per=true;
+	        break;
+	    }
+	    System.out.println(per);
+	
+		
+		
+		
+		
+		
+		
+	}
+}
