010d63121e40ce89129f7bb5f12512129c63ead4
jrathore07 <143330597+jrathore07@users.noreply.github.com>
2023-09-13 12:27:42 +0530

    Add files via upload

---
+++ CombineString.java
@@ -0,0 +1,20 @@
+//convert the string from lower case to upper case or vicevers
+import java.util.Scanner;
+public class CombineString
+{
+	public static void main(String[] args) {
+	    String s1="hello";
+		String s2="bye";
+		System.out.println("first String" + s1);
+		System.out.println("second String" + s2);
+		System.out.println("combinedstring " + s1+s2);
+		System.out.println("combinedstring " + s1+s2);
+			
+			String x=s1.concat(s2);
+			System.out.println("combinedstring "+x);
+			
+		
+		
+		
+	}
+}
