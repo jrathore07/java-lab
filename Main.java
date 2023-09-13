382c4073485c9ed890fe83345193ed0f593815d0
jrathore07 <143330597+jrathore07@users.noreply.github.com>
2023-09-13 11:41:49 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+		String first ="java";
+		String second ="python";
+		String third ="javaScript";
+		System.out.println(first);
+		System.out.println(second);
+		System.out.println(third);
+	}
+}
