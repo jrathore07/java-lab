94b0625b5a8318b3980112820f5159f517e05110
jrathore07 <143330597+jrathore07@users.noreply.github.com>
2023-09-13 12:46:50 +0530

    Add files via upload

---
+++ CompareString.java
@@ -0,0 +1,23 @@
+//compare two string
+import java.util.Scanner;
+public class CompareString
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    System.out.println("enter s1");
+	    String s1=sc.nextLine();
+	    System.out.println("enter s2");
+	    String s2=sc.nextLine();
+	   boolean r=s1.equals(s2);
+	   System.out.println(" "+r);
+	   
+	    
+	
+		
+		
+		
+		
+		
+		
+	}
+}
