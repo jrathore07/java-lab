8a3088918f8f65a7e397fd03b5443c50a6fd51f0
jrathore07 <143330597+jrathore07@users.noreply.github.com>
2023-10-06 15:47:40 +0530

    Add files via upload

---
+++ LinearSearchExample (1).java
@@ -0,0 +1,16 @@
+public class LinearSearchExample
+{
+	public static int LinearSearch(int[]arr,int key){
+	    for(int i=0;i<arr.length;i++){
+	        if(arr[i]==key){
+	            return i;
+	        }
+	    }
+	    return -1;
+	}
+	public static void main(String[] args){
+	    int []a1={10,20,30,50,70,90};
+	    int key = 50;
+	    System.out.println(key+"is found at index:"+LinearSearch(a1,key));
+	}
+}
