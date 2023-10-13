00b909f88507b4f492e5a13c27d12b6538d49180
jrathore07 <143330597+jrathore07@users.noreply.github.com>
2023-10-13 21:58:36 +0530

    Add files via upload

---
+++ Finalvariable.java
@@ -0,0 +1,13 @@
+
+class A {
+    final int AGE =35;
+    }
+    public class Finalvariable extends A{
+    public static void main(String[]args){
+       A a=new A();
+       a.AGE=45;
+        
+        
+        System.out.println("AGE"+a.AGE);
+    }
+}
