cf82e2a4d51546aa15c69e5d3c914fbfcdf1b29b
jrathore07 <143330597+jrathore07@users.noreply.github.com>
2023-10-13 15:49:45 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,12 +1,18 @@
 
+import java.util.*;
 public class Main
 {
 	public static void main(String[] args) {
-		String first ="java";
-		String second ="python";
-		String third ="javaScript";
-		System.out.println(first);
-		System.out.println(second);
-		System.out.println(third);
+		int a[]=new int[10];
+		Scanner sc=new Scanner(System.in);
+		for(int i=0;i<a.length;i++)
+		{
+		    System.out.println("Enter No.: ");
+		    a[i]=sc.nextInt();
+		}
+		for(int i=0;i<a.length;i++)
+		{
+		    System.out.println(a[i]);
+		}
 	}
 }
